5e61c006506391d3d6b59dd1a95701f0c7a86f61
hachiya433 <hachiya@adatype.co.jp>
2024-11-18 00:10:48 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2024_11_17_140558_create_category_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete(); // 外部キー: posts.id
+        $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete(); // 外部キー: categories.id
+        $table->primary(['post_id', 'category_id']); // 複合主キー
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_posts');
+    }
+};
